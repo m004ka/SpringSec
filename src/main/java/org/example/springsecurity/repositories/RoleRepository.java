67431a22c7fe0029ead00b3450855b2446e7c01d
m004ka <ysupov959@gmail.com>
2025-03-08 10:23:40 +0300

    update

--- src/main/java/org/example/springsecurity/repositories/RoleRepository.java
+++ src/main/java/org/example/springsecurity/repositories/RoleRepository.java
@@ -3,10 +3,9 @@ package org.example.springsecurity.repositories;
 
 import org.example.springsecurity.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role getRoleByRole(Role.Roles roles);
-
-
-
 }
