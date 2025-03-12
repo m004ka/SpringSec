0ac46125f2305de82a0b15e6f2189b100039b8dc
m004ka <ysupov959@gmail.com>
2025-03-12 03:24:00 +0300

    новые рест контроллеры

---
+++ src/main/java/org/example/springsecurity/controller/restController/AdminRestController.java
@@ -0,0 +1,12 @@
+package org.example.springsecurity.controller.restController;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/api/admin")
+@RequiredArgsConstructor
+public class AdminRestController {
+}
